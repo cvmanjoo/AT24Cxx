766a32fa23b3c8f7be56568fb96186013942f434
Manjunath CV <cvmanjoo@gmail.com>
2016-02-16 00:49:36 +0530

    Initial Commit
    Basic Library Structure.

---
+++ AT24Cxx.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   AT24Cxx.cpp
+ * Author: Manjunath CV
+ * 
+ * Created on February 16, 2016, 12:19 AM
+ */
+
+#include "AT24Cxx.h"
+
+AT24Cxx::AT24Cxx() {
+}
+
+AT24Cxx::AT24Cxx(const AT24Cxx& orig) {
+}
+
+AT24Cxx::~AT24Cxx() {
+}
+
---
+++ AT24Cxx.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   AT24Cxx.h
+ * Author: Manjunath CV
+ *
+ * Created on February 16, 2016, 12:19 AM
+ */
+
+#ifndef AT24CXX_H
+#define AT24CXX_H
+
+class AT24Cxx {
+public:
+    AT24Cxx();
+    AT24Cxx(const AT24Cxx& orig);
+    virtual ~AT24Cxx();
+private:
+
+};
+
+#endif /* AT24CXX_H */
+
